579e585ab4539438f1902ff6c275140e29e6b452
Yurii Mezentsev <yvmezentsev@gmail.com>
2023-01-16 20:28:17 +0200

    Add package lesson3

--- src/com/ua/robot/lesson3/Main.java
+++ src/com/ua/robot/lesson3/Main.java
@@ -1,7 +1,10 @@
-package com.ua.lesson2;
+package com.ua.robot.lesson3;
+
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!!!");
+
+
     }
 }
