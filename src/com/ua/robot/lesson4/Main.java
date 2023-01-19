ea1dd724f688e0685da37a8e83b6be9406012b66
Yurii Mezentsev <yvmezentsev@gmail.com>
2023-01-19 20:18:55 +0200

    Homework lesson4

---
+++ src/com/ua/robot/lesson4/Main.java
@@ -0,0 +1,12 @@
+package com.ua.robot.lesson4;
+
+public class Main {
+    public static void main(String[] args) {
+        String str = "Hello world!";
+
+        System.out.println(str.toUpperCase());
+        System.out.println(str.toLowerCase());
+        System.out.println(str.charAt(0));
+        System.out.println(str.charAt(str.length()-1));
+    }
+}
