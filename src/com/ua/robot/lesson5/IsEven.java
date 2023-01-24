26bfc80cbc74a0a95f0769760b9669b986dcde03
Yurii Mezentsev <yvmezentsev@gmail.com>
2023-01-24 14:51:25 +0200

    Homework lesson5

---
+++ src/com/ua/robot/lesson5/IsEven.java
@@ -0,0 +1,13 @@
+package com.ua.robot.lesson5;
+
+public class IsEven {
+    public static void main(String[] args) {
+        int number = 10;
+        if (number % 2 == 0) {
+            System.out.println("Number = " + number + " is even");
+        }
+        else {
+            System.out.println("Number = " + number + " is odd");
+        }
+    }
+}
